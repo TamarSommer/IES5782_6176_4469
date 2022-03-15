f41c5ad96171f7c76cbbab524188e92739a6e92f
DVORA-AZARKOVICH <azerkovi@g.jct.ac.il>
2022-03-15 18:09:13 +0200

    let me see you////

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -1,11 +1,19 @@
 package geometries;
 
 import primitives.Point;
+import primitives.Ray;
 import primitives.Vector;
+
+import java.util.List;
 
 public class Geometries implements Geometry{
     @Override
     public Vector getNormal(Point p) {
+        return null;
+    }
+
+    @Override
+    public List<Point> findIntersections(Ray ray) {
         return null;
     }
 }
