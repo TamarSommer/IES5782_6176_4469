e63035c3eb85cc2addaede731942e4bc2ec7890e
DVORA-AZARKOVICH <azerkovi@g.jct.ac.il>
2022-03-15 17:28:35 +0200

    ff

---
+++ src/geometries/Geometries.java
@@ -0,0 +1,11 @@
+package geometries;
+
+import primitives.Point;
+import primitives.Vector;
+
+public class Geometries implements Geometry{
+    @Override
+    public Vector getNormal(Point p) {
+        return null;
+    }
+}
